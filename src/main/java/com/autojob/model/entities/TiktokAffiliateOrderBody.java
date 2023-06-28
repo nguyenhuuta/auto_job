2ebde7da3bd49f382efa1396efd9b0130522b99c
Nguyen Huu Ta <nguyenhuuta456@gmail.com>
2023-06-28 13:54:53 +0700

    fix bug

--- src/main/java/com/autojob/model/entities/TiktokAffiliateOrderBody.java
+++ src/main/java/com/autojob/model/entities/TiktokAffiliateOrderBody.java
@@ -5,7 +5,22 @@ package com.autojob.model.entities;
  */
 public class TiktokAffiliateOrderBody {
     public String orderId;
-    public Integer sourceOrder;
     public String affiliateId;
     public Integer commissionDiscount;
+    /**
+     * -1, 0: đơn tự nhiên
+     * 1: livestream
+     * 2: video
+     */
+    public Integer sourceOrder;
+
+    @Override
+    public String toString() {
+        return "TiktokAffiliateOrderBody{" +
+                "orderId='" + orderId + '\'' +
+                ", affiliateId='" + affiliateId + '\'' +
+                ", commissionDiscount=" + commissionDiscount +
+                ", sourceOrder=" + sourceOrder +
+                '}';
+    }
 }
